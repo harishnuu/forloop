37610eceea6c318a4dbaf2b8efb915bb1b58a787
HP <HP@BOTNIMOUS>
2022-11-01 23:23:57 +0530

    Add project files.

---
+++ forloop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] myArray = { 'H', 'e', 'l', 'l', 'o' };
+            foreach (char ch in myArray)
+            {
+                Console.WriteLine(ch);
+            }
+            Console.ReadLine();
+        }
+    }
+}
